59f848b7596242433c7af6806f8f7dde8d39e338
Eduardo Alejo <eduardo.br@tecjerez.edu.mx>
2024-03-16 22:39:50 -0600

    Primer commit

---
+++ src/ventanaPrincipal.py
@@ -0,0 +1,13 @@
+'''
+Created on 16 mar 2024
+
+@author: ed308
+'''
+from tkinter import Tk
+
+
+interfaz = Tk()
+interfaz.title("Calculadora en PYTHON")
+interfaz.geometry("400x600")
+
+interfaz.mainloop()
